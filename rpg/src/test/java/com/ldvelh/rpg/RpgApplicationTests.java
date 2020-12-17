e8871f81b8ca78ecc8fd81c93156f9bb0ed6a080
pitchounvivi <pitchounvivi@gmail.com>
2020-12-17 09:26:53 +0100

    Ajout Spring boot

---
+++ rpg/src/test/java/com/ldvelh/rpg/RpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldvelh.rpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
